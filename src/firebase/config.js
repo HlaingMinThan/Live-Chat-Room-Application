e02d8cde0ad365ca660c2ef6121609f5b70cbd29
HlaingMinThan <hlaingminthan92@gmail.com>
2020-12-25 23:44:37 +0630

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+import "firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyAUo7hHsMS-3DCEyh1Okk9b9Y4AiPnOBw8",
+    authDomain: "vue-blog-system-2ca2e.firebaseapp.com",
+    projectId: "vue-blog-system-2ca2e",
+    storageBucket: "vue-blog-system-2ca2e.appspot.com",
+    messagingSenderId: "691843420825",
+    appId: "1:691843420825:web:59e8ccb790193b13335e08"
+  };
+  firebase.initializeApp(firebaseConfig);
+  let db=firebase.firestore();
+  let timestamp=firebase.firestore.FieldValue.serverTimestamp;
+
+  export {db,timestamp};
